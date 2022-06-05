24bc8a73fc606b10390eeac8668308b43fa56fd4
kirillnovytskyi <kirill.novytskyi7123@gmail.com>
2022-06-05 16:59:49 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Header } from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="component-container">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
